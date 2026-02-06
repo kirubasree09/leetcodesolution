b08b171e2e54c6c5d8e0d749fa04d9611e4c844c
kirubasree09 <24149025@drngpit.ac.in>
2026-02-06 14:24:17 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.java
@@ -0,0 +1,12 @@
+// Last updated: 2/6/2026, 2:24:16 PM
+class Solution {
+    public int reverseBits(int n) {
+        int a = 0;
+    for(int i = 0; i<32; i++ ){
+        a<<=1;
+        a=(a|(n & 1));
+        n>>=1;
+    }
+    return a;
+    }
+}
