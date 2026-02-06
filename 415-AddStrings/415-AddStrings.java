e970589de0f44e0d35411a75a1580587b0611b54
kirubasree09 <24149025@drngpit.ac.in>
2026-02-06 14:23:58 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.java
@@ -0,0 +1,9 @@
+// Last updated: 2/6/2026, 2:23:57 PM
+import java.math.BigInteger;
+class Solution {
+    public String addStrings(String num1, String num2) {
+        BigInteger a=new BigInteger(num1);
+        BigInteger b=new BigInteger(num2);
+        return a.add(b).toString();
+    }
+}
